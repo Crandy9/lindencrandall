32427086f6b7c14c5546c2806636890aed0132d6
Linden <lindencrandall123@gmail.com>
2021-07-09 10:58:55 +0900

    Added ThreeJs and WebGL pages

---
+++ threejsdemo.js
@@ -0,0 +1,300 @@
+//global vars
+let scene, camera, renderer,line,canvas,torusKnot, 
+    smallCube, smallCube2, core, smallCone1,smallcone2, 
+    coneModel1,coneModel2, centerModel, starSphere, stars=[],
+    lightsOnRadio, lightsOffRadio, newLight, light,upLight,
+    rightLight,leftLight, orbitSphere;
+
+//initialize scene, camera, renderer, etc.
+function init() {
+
+    //scene that all the models and cameras, etc. will be added to
+    scene = new THREE.Scene();
+    /*
+        first param of camera is the camera frustum vertical fov
+        second param is aspect ratio
+        third and fourth params are camera near and far frustum
+    */
+    camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight, 0.1,1000);
+    //set camera position inside sphere planet
+    camera.position.set(0,0,5);
+    camera.lookAt(new THREE.Vector3(0,0,0));
+    /**
+     * lights will added to camera/scene when function turnOn or turnOff is called
+     */
+    //light blue light attached to the camera goes at 0.6 intensity
+    light = new THREE.PointLight(0x57f6f1,0.6);
+    //red light pointing up from point (0,-10,0)
+    upLight = new THREE.DirectionalLight("red", 5);
+    upLight.position.set(0,-10,0);
+
+    //right light pointing to the right from point (10,0,0)
+    rightLight = new THREE.DirectionalLight("green", 0.5);
+    rightLight.position.set(10,0,0);
+
+    //left light pointing to the left from point (-10,0,0)
+    leftLight = new THREE.DirectionalLight("blue", 5);
+    leftLight.position.set(-10,0,0);
+
+    /**
+     * renderer
+     */
+
+    //renderer is what draws the scene
+    renderer = new THREE.WebGLRenderer({canvas: canvas,antialias: true});
+    //resizes output canvas. min size for project3 is 640x480
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    //set background color
+    renderer.setClearColor("black");
+    document.body.appendChild(renderer.domElement);
+    //used from OrbitControls.js. Allows mouse-dragging, wheel zoom in and out
+    var controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.update();
+
+    //cone model1 and model2
+    coneModel1 = new THREE.Object3D();
+    coneModel2 = new THREE.Object3D();
+    //this is used to perform animation transforms on both cones as a single unit
+    centerModel = new THREE.Object3D();
+    //create cone1
+    const coneGeo1 = new THREE.ConeGeometry(1,2,8,30);
+    const coneMaterial1 = new THREE.MeshLambertMaterial({
+        color: "white"
+    })
+    smallCone1 = new THREE.Mesh(coneGeo1,coneMaterial1);
+    smallCone1.position.y = 1;
+    coneModel1.add(smallCone1);
+    centerModel.add(coneModel1);
+
+    //create cone2
+    const coneGeo2 = new THREE.ConeGeometry(1,2,8,30);
+    const coneMaterial2 = new THREE.MeshLambertMaterial({
+        color: "white"
+    })
+    smallCone2 = new THREE.Mesh(coneGeo2,coneMaterial2);
+    smallCone2.scale.y = -1;
+    smallCone2.position.y = -2;
+    coneModel2.add(smallCone2);
+    centerModel.add(coneModel2);
+
+    //adding both cones as a single unit to the scene
+    scene.add(centerModel);
+    //create core
+    const coreGeo = new THREE.SphereGeometry(.5,32,32);
+    const coreMat = new THREE.MeshBasicMaterial({
+        color: 0xa60707
+
+    });
+    core = new THREE.Mesh(coreGeo,coreMat);
+    core.position.y = -0.5;
+    centerModel.add(core);
+
+    //create orbitSphere
+    const orbitGeo = new THREE.SphereGeometry(1,32,32);
+    const orbitMat  = new THREE.MeshBasicMaterial( { 
+        color: "blue",
+    });
+    orbitSphere = new THREE.Mesh(orbitGeo,orbitMat);
+    orbitSphere.position.y = -11;
+    scene.add(orbitSphere);
+
+    //create torus knot
+    const torusGeometry = new THREE.TorusKnotGeometry(10,1);
+    const torusMat = new THREE.MeshLambertMaterial( { 
+            color: 0x227d10,
+            wireframe:true
+    });
+    torusKnot = new THREE.Mesh( torusGeometry, torusMat );
+    //position it above the sphere along the y-axis
+    torusKnot.position.y = 15;
+    torusKnot.scale.set(0.2,0.2,.5);
+    scene.add(torusKnot);
+
+
+    //create small box
+    const smallBoxGeo = new THREE.BoxGeometry(1,1,1);
+    const boxMaterial = new THREE.MeshLambertMaterial({
+        color: "white",
+        wireframe: false
+    });
+    smallCube = new THREE.Mesh(smallBoxGeo, boxMaterial);
+    scene.add(smallCube);
+
+    //create small box2
+    const smallBoxGeo2 = new THREE.BoxGeometry(1,1,1);
+    const boxMaterial2 = new THREE.MeshLambertMaterial({
+        color: "white",
+        wireframe: false
+    });
+    smallCube2 = new THREE.Mesh(smallBoxGeo2, boxMaterial2);
+    scene.add(smallCube2);
+
+    //create wireframe sphere
+    const sphereGeo = new THREE.SphereGeometry(10,20,100);
+    const wireFrame = new THREE.WireframeGeometry(sphereGeo);
+    line = new THREE.LineSegments(wireFrame);
+    line.material.depthTest =true;
+    line.material.opacity = 0.25;
+    line.material.transparent = true;
+    scene.add(line);
+
+    /**
+     * widget controls
+     */
+    //adding event listener to radio buttons 
+     lightsOnRadio = document.getElementById("lightsOn");
+     lightsOnRadio.addEventListener("change", turnOn);
+
+     lightsOffRadio = document.getElementById("lightsOff");
+     lightsOffRadio.addEventListener("change", turnOff);
+}//end init function
+
+
+//methods to turn lights on or off
+function turnOff() {
+    console.log("lights off");
+        camera.remove(light);
+        scene.remove(upLight);
+        scene.remove(rightLight);
+        scene.remove(leftLight);
+        render();
+}
+
+function turnOn() {
+    console.log("lights on");
+        camera.add(light);
+        scene.add(upLight);
+        scene.add(rightLight);
+        scene.add(leftLight);
+        render();
+}
+
+/**
+ * adding starfield
+ */
+function addSphere(){
+
+    // The loop will move from z position of -1000 to z position 1000, adding a random particle at each position. 
+    for ( var z= -4000; z < 4000; z+=30) {
+
+        // Make a sphere (exactly the same as before). 
+        var geometry   = new THREE.SphereGeometry(0.5, 32, 32)
+        var material = new THREE.MeshBasicMaterial( {color: 0xffffff} );
+         starSphere = new THREE.Mesh(geometry, material)
+
+        // This time we give the sphere random x and y positions between -500 and 500
+        starSphere.position.x = Math.random() * 1000-500;
+        starSphere.position.y = Math.random() * 1000-500;
+
+        // Then set the z position to where it is in the loop (distance of camera)
+        starSphere.position.z = z;
+
+        //add the sphere to the scene
+        scene.add( starSphere );
+
+        //finally push it to the stars array 
+        stars.push(starSphere); 
+    }
+}
+
+//animate starfield
+function animateStars() { 
+    
+    // loop through each star
+    for(var i=0; i<stars.length; i++) {
+
+        star = stars[i]; 
+            
+        // and move it forward
+        star.position.z += i/10;
+            
+        // if the particle is too close move it to the back
+        if(star.position.z>100) star.position.z-=2000; 
+
+    }
+
+}
+
+
+
+//this function animates the scene
+function animate() {
+    //using a timestamp for animations
+    var myTime = Date.now() * 0.0001;
+    //creates an infinite loop that makes renderer
+    //redraw scene everytime screen is refreshed which is about 60fps
+    requestAnimationFrame(animate);
+
+
+    //animate top conemodel
+    coneModel1.position.y = Math.sin(myTime*10) * 0.5;
+    //animate bottom conemodel
+    coneModel2.position.y = -Math.sin(myTime*10) * 0.5;
+
+    //animate cones
+    smallCone1.rotation.y = myTime*50;
+    smallCone2.rotation.y = myTime*50;
+
+    //animate centerModel
+   centerModel.rotation.x = (myTime * 10) * 5;
+   centerModel.rotation.z = (myTime * 10);
+    //wireframe planet animation
+     line.rotation.x +=0.01;
+     line.rotation.y +=0.01;
+     line.rotation.z +=0.01;
+
+    //inner small box animation
+    //bounce around inside of sphere planet
+    smallCube.position.x = Math.cos(myTime*20) * 5;
+    smallCube.position.z = Math.cos(myTime*20) * 4;
+    smallCube.position.z = Math.sin(myTime*20) * -4;
+    //rotate the cube
+    smallCube.rotation.x += 0.03;
+    smallCube.rotation.y += 0.09;
+    smallCube.rotation.z += 0.03;
+
+    //inner small box2 animation
+    //bounce around inside of sphere planet
+    smallCube2.position.x = -Math.cos(myTime*20) * 5;
+    smallCube2.position.z = -Math.cos(myTime*20) * 4;
+    smallCube2.position.z = -Math.sin(myTime*20) * -4;
+    //rotate the cube
+    smallCube2.rotation.x -= 0.03;
+    smallCube2.rotation.y -= 0.09;
+    smallCube2.rotation.z -= 0.03;
+
+
+    //torus animation
+    /**
+     *  rotation around mesh sphere planet
+        cos creates the back and forth effect along x axis
+        sin creates up and down effect along y axis, 
+        myTime is the current timestamp multiplied by the speed (x and y should match)
+        the last term is the height and length of the orbit
+     */
+    torusKnot.position.x = -Math.cos(myTime*4) * 15;
+    torusKnot.position.y = Math.sin(myTime*4) * 15;
+    //rotating the torus itself
+    torusKnot.rotation.y += 0.12;
+
+    //orbitSphere animation
+    orbitSphere.position.x = Math.cos(myTime*30) * 10;
+    orbitSphere.position.y = Math.cos(myTime*30) * 10;
+    orbitSphere.position.z = -Math.sin(myTime*30) * 15;
+
+    //finally, draw the scene
+    render();
+}
+
+//this function draws the scene
+function render() {
+    renderer.render(scene, camera);
+    animateStars();
+}
+
+//initialize scene
+init();
+//add star spheres
+addSphere();
+//call the function
+animate();
---
+++ webgldemo.js
@@ -0,0 +1,462 @@
+"use strict";
+
+var gl;   // The webgl context.
+var zval = 0;
+var yval = 0;
+var a_coords_loc;         // Location of the a_coords attribute variable in the shader program.
+var a_normal_loc;         // Location of a_normal attribute 
+var a_texCoords_loc; 
+
+var u_modelview;       // Locations for uniform matrices
+var u_projection;
+var u_normalMatrix;
+
+var u_material;     // An object tolds uniform locations for the material.
+var u_lights;       // An array of objects that holds uniform locations for light properties.
+
+var projection = mat4.create();    // projection matrix
+var modelview;                     // modelview matrix; value comes from rotator
+var normalMatrix = mat3.create();  // matrix, derived from modelview matrix, for transforming normal vectors
+
+var rotator;  // A TrackballRotator to implement rotation by mouse.
+
+ var frameNumber = 0;  // frame number during animation (actually only goes up by 0.5 per frame)
+
+var torus, sphere, cone, cylinder, disk, ring, cube;  // basic objects, created using function createModel
+
+var matrixStack = [];           // A stack of matrices for implementing hierarchical graphics.
+
+var currentColor = [1,1,1,1];   // The current diffuseColor; render() functions in the basic objects set
+                                // the diffuse color to currentColor when it is called before drawing the object.
+                                // Other color properties, which don't change often are handled elsewhere.
+
+
+/**
+ *  VBO function used from diskplay.html
+ */
+function createModel(modelData, xtraTranslate) {
+    var model = {};
+    model.coordsBuffer = gl.createBuffer();
+    model.normalBuffer = gl.createBuffer();
+    model.indexBuffer = gl.createBuffer();
+    model.count = modelData.indices.length;
+    if (xtraTranslate)
+        model.xtraTranslate = xtraTranslate;
+    else
+        model.xtraTranslate = null;
+    gl.bindBuffer(gl.ARRAY_BUFFER, model.coordsBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, modelData.vertexPositions, gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, model.normalBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, modelData.vertexNormals, gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, model.indexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, modelData.indices, gl.STATIC_DRAW);
+    model.render = function() { 
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.coordsBuffer);
+        gl.vertexAttribPointer(a_coords_loc, 3, gl.FLOAT, false, 0, 0);
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.normalBuffer);
+        gl.vertexAttribPointer(a_normal_loc, 3, gl.FLOAT, false, 0, 0);
+        gl.uniform4fv(u_material.diffuseColor, currentColor);
+        if (this.xtraTranslate) {
+            pushMatrix();
+            mat4.translate(modelview,modelview,this.xtraTranslate);
+        }
+        gl.uniformMatrix4fv(u_modelview, false, modelview );
+        mat3.normalFromMat4(normalMatrix, modelview);
+        gl.uniformMatrix3fv(u_normalMatrix, false, normalMatrix);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
+        gl.drawElements(gl.TRIANGLES, this.count, gl.UNSIGNED_SHORT, 0);
+        if (this.xtraTranslate) {
+            popMatrix();
+        }
+    }
+    return model;
+}
+
+//--------------------------------- animation framework ------------------------------
+
+    
+var animating = false;
+
+/*
+This is where you control the animation by changing positions,
+and rotations values as needed.
+Trial and error works on the numbers. Graph paper design is more efficient. 
+*/
+
+function frame() {
+    if (animating) {
+        //these four variables perform slightly different animations. Gotta love trig!
+        frameNumber += 1;
+        var myTime = performance.now()* 0.001;
+        yval += Math.sin(myTime) / 10;
+        zval -= Math.cos(myTime) / 10;
+        draw();
+        requestAnimationFrame(frame);
+    }
+}
+
+//called when animate checkbox is selected
+function setAnimating(run) {
+    if (run != animating) {
+        animating = run;
+        if (animating)
+            requestAnimationFrame(frame);
+    }
+}
+
+//-------------------------------------------------------------------------
+
+
+/**
+ * initialization
+ */
+function init() {
+    try {
+        var canvas = document.getElementById("myCanvas");
+        gl = canvas.getContext("webgl") || 
+                         canvas.getContext("experimental-webgl");
+        if ( ! gl ) {
+            throw "Browser does not support WebGL";
+        }
+    }
+    catch (e) {
+        document.getElementById("message").innerHTML =
+            "<p>Sorry, could not get a WebGL graphics context.</p>";
+        return;
+    }
+    try {
+        initGL();  // initialize the WebGL graphics context
+    }
+    catch (e) {
+        document.getElementById("message").innerHTML =
+            "<p>Sorry, could not initialize the WebGL graphics context:" + e + "</p>";
+        return;
+    }
+    document.getElementById("animCheck").checked = false;
+    document.getElementById("reset").onclick = function() {
+       rotator.setView(17,[0,1,2]);
+       //reset to default position
+        frameNumber = 0;
+        zval = 0;
+        yval = 0;
+       animating = false;
+       document.getElementById("animCheck").checked = false;
+       draw();
+    }
+    
+    // Create all the basic objects.
+    torus = createModel(uvTorus(0.5,1,16,8)); 
+    sphere = createModel(uvSphere(1));
+    cone = createModel(uvCone(),[0,0,.5]);
+    cylinder = createModel(uvCylinder(),[0,0,1.5]);
+    disk = createModel(uvCylinder(5.5,0.5,64),[0,0,.25]);
+    ring = createModel(ring(3.3,4.8,40));
+    cube = createModel(cube());
+ 
+ // This controls the zoom and initial placement
+    rotator = new TrackballRotator(canvas,function() {
+        if (!animating)
+           draw();
+    },17,[0,1,2]); 
+    draw();
+}
+
+/* Initialize the WebGL context.  Called from init() */
+function initGL() {
+    var prog = createProgram(gl,"vshader-source","fshader-source");
+    gl.useProgram(prog);
+    gl.enable(gl.DEPTH_TEST);
+    
+    /* Get attribute and uniform locations */
+    
+    a_coords_loc =  gl.getAttribLocation(prog, "a_coords");
+    a_normal_loc =  gl.getAttribLocation(prog, "a_normal");
+    gl.enableVertexAttribArray(a_coords_loc);
+    gl.enableVertexAttribArray(a_normal_loc);
+    
+    u_modelview = gl.getUniformLocation(prog, "modelview");
+    u_projection = gl.getUniformLocation(prog, "projection");
+    u_normalMatrix =  gl.getUniformLocation(prog, "normalMatrix");
+    u_material = {
+        diffuseColor: gl.getUniformLocation(prog, "material.diffuseColor"),
+        specularColor: gl.getUniformLocation(prog, "material.specularColor"),
+        emissiveColor: gl.getUniformLocation(prog, "material.emissiveColor"),
+        specularExponent: gl.getUniformLocation(prog, "material.specularExponent")
+    };
+    u_lights = new Array(4);
+    for (var i = 0; i < 4; i++) {
+        u_lights[i] = {
+            enabled: gl.getUniformLocation(prog, "lights[" + i + "].enabled"),
+            position: gl.getUniformLocation(prog, "lights[" + i + "].position"),
+            color: gl.getUniformLocation(prog, "lights[" + i + "].color")            
+        };
+    }
+            
+    gl.uniform3f( u_material.specularColor, 0.1, 0.1, 0.1 );  // specular properties don't change
+    gl.uniform1f( u_material.specularExponent, 16 );
+    gl.uniform3f( u_material.emissiveColor, 0, 0, 0);  // default, will be changed temporarily for some objects
+    
+
+    for (var i = 1; i < 4; i++) { // set defaults for lights
+        gl.uniform1i( u_lights[i].enabled, 0 ); 
+        gl.uniform4f( u_lights[i].position, 0, 0, 1, 0 );        
+        gl.uniform3f( u_lights[i].color, 1,1,1 ); 
+    }
+    
+  // Lights are set on in the draw() method 
+} // end initGL()
+
+/* Creates a program for use in the WebGL context gl, and returns the
+ * identifier for that program.  If an error occurs while compiling or
+ * linking the program, an exception of type String is thrown.  The error
+ * string contains the compilation or linking error.  If no error occurs,
+ * the program identifier is the return value of the function.
+ *    The second and third parameters are the id attributes for <script>
+ * elementst that contain the source code for the vertex and fragment
+ * shaders.
+ */
+function createProgram(gl, vertexShaderID, fragmentShaderID) {
+    function getTextContent( elementID ) {
+            // This nested function retrieves the text content of an
+            // element on the web page.  It is used here to get the shader
+            // source code from the script elements that contain it.
+        var element = document.getElementById(elementID);
+        var node = element.firstChild;
+        var str = "";
+        while (node) {
+            if (node.nodeType == 3) // this is a text node
+                str += node.textContent;
+            node = node.nextSibling;
+        }
+        return str;
+    }
+    try {
+        var vertexShaderSource = getTextContent( vertexShaderID );
+        var fragmentShaderSource = getTextContent( fragmentShaderID );
+    }
+    catch (e) {
+        throw "Error: Could not get shader source code from script elements.";
+    }
+    var vsh = gl.createShader( gl.VERTEX_SHADER );
+    gl.shaderSource(vsh,vertexShaderSource);
+    gl.compileShader(vsh);
+    if ( ! gl.getShaderParameter(vsh, gl.COMPILE_STATUS) ) {
+        throw "Error in vertex shader:  " + gl.getShaderInfoLog(vsh);
+     }
+    var fsh = gl.createShader( gl.FRAGMENT_SHADER );
+    gl.shaderSource(fsh, fragmentShaderSource);
+    gl.compileShader(fsh);
+    if ( ! gl.getShaderParameter(fsh, gl.COMPILE_STATUS) ) {
+       throw "Error in fragment shader:  " + gl.getShaderInfoLog(fsh);
+    }
+    var prog = gl.createProgram();
+    gl.attachShader(prog,vsh);
+    gl.attachShader(prog, fsh);
+    gl.linkProgram(prog);
+    if ( ! gl.getProgramParameter( prog, gl.LINK_STATUS) ) {
+       throw "Link error in program:  " + gl.getProgramInfoLog(prog);
+    }
+    return prog;
+}
+
+/**
+ * Draws the image, which consists of either the "world" or a closeup of the "car".
+ */
+ function draw() {
+     //black background
+    gl.clearColor(.224,.224,.224,1);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    
+    
+    mat4.perspective(projection, Math.PI/3, 1, 1, 100);
+    gl.uniformMatrix4fv(u_projection, false, projection );
+
+    modelview = rotator.getViewMatrix();
+    
+    lights();
+    
+    shapes();
+}
+
+function lights() {   
+     
+    // Three of four lights used, all enabled
+    // Use lights to enhance models looks
+    gl.uniform1i( u_lights[0].enabled, 1 );   
+    // Looking down z
+    gl.uniform4f( u_lights[0].position, 0,0,1,0 ); 
+    gl.uniform3f( u_lights[0].color, 0,0,1 );  //blue front
+    
+    gl.uniform1i( u_lights[1].enabled, 1 );  
+    // Looking down X
+    gl.uniform4f( u_lights[1].position, 1,0,0,0 ); 
+    gl.uniform3f( u_lights[1].color, 1,1,1 );  //white right
+    
+     gl.uniform1i( u_lights[2].enabled, 1 );  
+    // Looking down Y
+    gl.uniform4f( u_lights[2].position, 0,1,0,0 ); 
+    gl.uniform3f( u_lights[2].color, 1.0,1.0,0 );  //top red
+    
+    
+    currentColor = [ 0.3, 0.3, 0.3, 1 ];
+}
+
+/**
+ * Draws and animates each shape. This is my rendition of hierarchical modeling
+ */
+ function shapes() {
+
+    pushMatrix();//start hierarchy
+    //alter view so that camera position is centered in the middle of the ring struct
+    mat4.rotate(modelview,modelview, -0.45,[1,0,0]);
+    //rotate around the whole model
+    mat4.rotate(modelview,modelview, -0.45 * frameNumber / 20,[0,1,0]);
+
+    //sphere
+	pushMatrix();
+    //scale up and down animation
+    mat4.scale(modelview,modelview,[Math.sin(frameNumber / 100),Math.sin(frameNumber / 100),
+                                                                Math.sin(frameNumber / 100)]);
+    mat4.translate(modelview,modelview,[0, 0, 0]);
+	currentColor = [1,0,1,1];
+	sphere.render();
+	popMatrix();
+    
+	// outer ring
+    pushMatrix();
+	mat4.translate(modelview,modelview,[0,0,0]);
+    //uses vzal animation variable
+	mat4.rotate(modelview,modelview,zval,[1,1,0]);
+	mat4.scale(modelview,modelview,[1.3,1.3,1]);
+	currentColor = [1,0,0,1];
+	ring.render();
+	popMatrix();	
+
+    // inner ring
+    pushMatrix();
+	mat4.translate(modelview,modelview,[0,0,0]);
+    //uses a rendition of frameNumber to animate
+	mat4.rotate(modelview,modelview,-frameNumber / 5,[1,1,1]);
+	mat4.scale(modelview,modelview,[.9,.9,0]);
+	currentColor = [1, 1, 0, 1];
+	ring.render();
+	popMatrix();	
+	
+    /**
+     * four cubes at each corner of the canvas have rotation animations among theier axes
+     */
+    //top left cube
+	pushMatrix();
+	mat4.translate(modelview,modelview,[-5,5,0]);
+	mat4.rotate(modelview,modelview,frameNumber / 10,[1,1,1]);
+	mat4.scale(modelview,modelview,[1.0,.6,1.4]);
+	currentColor = [1,1,1,1];
+	cube.render();
+	popMatrix();	
+
+    // top right cube
+	pushMatrix();
+	mat4.translate(modelview,modelview,[5,5,0]);
+	mat4.rotate(modelview,modelview,frameNumber / 10,[1,1,1]);
+	mat4.scale(modelview,modelview,[1.0,.6,1.4]);
+	currentColor = [0.17,0,1,1];
+	cube.render();
+	popMatrix();	
+
+    // bottom left cube
+	pushMatrix();
+	mat4.translate(modelview,modelview,[-5,-5,0]);
+	mat4.rotate(modelview,modelview,frameNumber / 10,[1,1,1]);
+	mat4.scale(modelview,modelview,[1.0,.6,1.4]);
+	currentColor = [1,0.204,.204,1];
+	cube.render();
+	popMatrix();
+
+    //bottom right cube
+	pushMatrix();
+	mat4.translate(modelview,modelview,[5,-5,0]);
+	mat4.rotate(modelview,modelview,frameNumber / 10,[1,1,1]);
+	mat4.scale(modelview,modelview,[1.0,.6,1.4]);
+	currentColor = [1.55,1.53,.29,1];
+	cube.render();
+	popMatrix();
+
+    /**
+     * two white torus at each end of the z-axis and their animations
+     */
+	// white torus
+	pushMatrix();
+	mat4.translate(modelview,modelview,[0,0,-7]);
+	mat4.rotate(modelview,modelview,3.1416,[1,0,0]);
+    mat4.rotate(modelview,modelview,frameNumber / 4,[0,1,0]);
+	currentColor = [1, 1, 1,1];
+	torus.render();
+	popMatrix();	
+
+    // white torus
+	pushMatrix();
+	mat4.translate(modelview,modelview,[0,0,7]);
+	mat4.rotate(modelview,modelview,3.1416,[1,0,0]);
+    mat4.rotate(modelview,modelview,frameNumber / 4,[0,1,0]);
+	currentColor = [1, 1, 1,1];
+	torus.render();
+	popMatrix();
+	
+    /**
+     * two pairs of oblong objects that do a slow rotation animation
+     */
+    //bottom flat shape
+    pushMatrix();
+	mat4.translate(modelview,modelview,[0,-2,7]);
+    mat4.rotate(modelview, modelview, -frameNumber / 90, [1,1,0])
+	mat4.scale(modelview,modelview,[2,0.6,1]);
+	currentColor = [0.0,1.0,0.1,1];
+	cylinder.render();
+	popMatrix();	
+
+    //top flat shape
+    pushMatrix();
+    mat4.translate(modelview,modelview,[0,2,7]);
+    mat4.rotate(modelview, modelview, frameNumber / 90, [1,1,0])
+	mat4.scale(modelview,modelview,[2,0.6,1]);
+	currentColor = [0.0,1.0,0.1,1];
+	cylinder.render();
+	popMatrix();	
+
+
+    //bottom flat shape
+    pushMatrix();
+	mat4.translate(modelview,modelview,[0,-2,-7]);
+    mat4.rotate(modelview, modelview, -frameNumber / 90, [1,1,0])
+	mat4.scale(modelview,modelview,[2,0.6,1]);
+	currentColor = [0.0,1.0,0.1,1];
+	cylinder.render();
+	popMatrix();	
+
+    //top flat shape
+    pushMatrix();
+    mat4.translate(modelview,modelview,[0,2,-7]);
+    mat4.rotate(modelview, modelview, frameNumber / 90, [1,1,0])
+	mat4.scale(modelview,modelview,[2,0.6,1]);
+	currentColor = [0.0,1.0,0.1,1];
+	cylinder.render();
+	popMatrix();	
+
+    popMatrix();//end hierarchy
+	  	
+}
+
+/**
+ *  Push a copy of the current modelview matrix onto the matrix stack.
+ */
+ function pushMatrix() {
+    matrixStack.push( mat4.clone(modelview) );
+}
+
+
+/**
+ *  Restore the modelview matrix to a value popped from the matrix stack.
+ */
+function popMatrix() {
+    modelview = matrixStack.pop();
+}
